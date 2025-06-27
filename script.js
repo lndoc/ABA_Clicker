3d33cb7889a7de7ef8668a65934ec9385a94f819
lndoc <jordan09305@gmail.com>
2025-06-27 12:34:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ const upgrades = [
 
 function nameToFilename(name) {
     const fileName = name.replace(/[^a-zA-Z0-9]/g, '_') + '.png';
-    console.log(`https://lndoc.github.io/ABA_Clicker/upgrade_images/${fileName}`);
     return `https://lndoc.github.io/ABA_Clicker/upgrade_images/${fileName}`;
 }
 
@@ -250,7 +249,7 @@ document.addEventListener('DOMContentLoaded', () => {
     updateDisplay();
 });
 
-setInterval(saveGame, 5000); 
+setInterval(saveGame, 500); 
 
 document.querySelectorAll("*").forEach(el => {
     el.style.touchAction = "manipulation";
