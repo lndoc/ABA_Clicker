2a63f4f893cb626ea94f27c3cc9a263431344f30
lndoc <jordan09305@gmail.com>
2025-06-27 11:28:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,22 +9,22 @@ const upgradesContainer = document.querySelector('.upgrades');
 const passiveAbaDisplay = document.getElementById('passiveAba');
 
 const upgrades = [
-    { name: 'ABA Chan', baseCost: 1, abaPerClick: 0, passiveAba: 1, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/0/02/ABA_Chan.png/revision/latest?cb=20230823193536' },
-    { name: 'PTS Ichigo', baseCost: 10, abaPerClick: 1, passiveAba: 0.1, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/3/3e/Ichigo.png/revision/latest?cb=20200214021355' },
-    { name: 'Grimmjow', baseCost: 100, abaPerClick: 10, passiveAba: 1, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/e/ee/GrimmjowBase.png/revision/latest?cb=20211225151354' },
-    { name: 'Aizen', baseCost: 500, abaPerClick: 50, passiveAba: 5, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/6/69/Unknown_%2848%29.png/revision/latest?cb=20200607182320' },
-    { name: 'Yoruichi', baseCost: 1000, abaPerClick: 100, passiveAba: 10, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/7/79/Yoruichilook.PNG/revision/latest?cb=20210501122404' },
-    { name: 'PTS Luffy', baseCost: 2500, abaPerClick: 1000, passiveAba: 50, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/3/3d/Luffy.png/revision/latest?cb=20200212190829' },
-    { name: 'PTS Luffy', baseCost: 5000, abaPerClick: 500, passiveAba: 50, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/5/56/TS_luffy.PNG/revision/latest?cb=20200604060217' },
-    { name: 'PTS Naruto', baseCost: 10000, abaPerClick: 1000, passiveAba: 100, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/e/ea/Naruto.png/revision/latest?cb=20200212234650' },
-    { name: 'Nameku', baseCost: 50000, abaPerClick: 5000, passiveAba: 500, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/8/81/New_Goku_Skin_1.png/revision/latest?cb=20230201060418' },
-    { name: 'PTS Sasuke', baseCost: 100000, abaPerClick: 10000, passiveAba: 1000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/7/77/PTS_Sasuke_.png/revision/latest?cb=20250215030949' },
-    { name: 'Android Vegeta', baseCost: 200000, abaPerClick: 20000, passiveAba: 2000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/8/84/Vegeta_Skin_1.png/revision/latest?cb=20230402084519' },
-    { name: 'PTS Zoro', baseCost: 500000, abaPerClick: 20000, passiveAba: 10000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/b/b8/Zoro.PNG/revision/latest?cb=20200325195704' },
-    { name: 'TS Zoro', baseCost: 500000, abaPerClick: 50000, passiveAba: 5000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/4/48/ZoroS1.png/revision/latest?cb=20220827000102' },
-    { name: 'Saitama', baseCost: 1000000, abaPerClick: 100000, passiveAba: 10000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/1/12/Screen_Shot_2021-03-24_at_1.09.40_PM.png/revision/latest?cb=20210324171208' },
-    { name: 'Tanjiro', baseCost: 2000000, abaPerClick: 200000, passiveAba: 20000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/6/61/Tanjiro.png/revision/latest?cb=20200824190824' },
-    { name: 'Nanami', baseCost: 10000000, abaPerClick: 2500000, passiveAba: 100000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/c/c9/Nanamibase.png/revision/latest?cb=20241226040857' },
+    { name: 'ABA Chan', baseCost: 10, abaPerClick: 0, passiveAba: 1, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/0/02/ABA_Chan.png/revision/latest?cb=20230823193536' },
+    { name: 'PTS Ichigo', baseCost: 100, abaPerClick: 1, passiveAba: 0.1, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/3/3e/Ichigo.png/revision/latest?cb=20200214021355' },
+    { name: 'Grimmjow', baseCost: 1000, abaPerClick: 10, passiveAba: 1, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/e/ee/GrimmjowBase.png/revision/latest?cb=20211225151354' },
+    { name: 'Aizen', baseCost: 5000, abaPerClick: 50, passiveAba: 5, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/6/69/Unknown_%2848%29.png/revision/latest?cb=20200607182320' },
+    { name: 'Yoruichi', baseCost: 10000, abaPerClick: 100, passiveAba: 10, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/7/79/Yoruichilook.PNG/revision/latest?cb=20210501122404' },
+    { name: 'PTS Luffy', baseCost: 25000, abaPerClick: 1000, passiveAba: 50, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/3/3d/Luffy.png/revision/latest?cb=20200212190829' },
+    { name: 'PTS Luffy', baseCost: 50000, abaPerClick: 500, passiveAba: 50, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/5/56/TS_luffy.PNG/revision/latest?cb=20200604060217' },
+    { name: 'PTS Naruto', baseCost: 100000, abaPerClick: 1000, passiveAba: 100, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/e/ea/Naruto.png/revision/latest?cb=20200212234650' },
+    { name: 'Nameku', baseCost: 500000, abaPerClick: 5000, passiveAba: 500, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/8/81/New_Goku_Skin_1.png/revision/latest?cb=20230201060418' },
+    { name: 'PTS Sasuke', baseCost: 1000000, abaPerClick: 10000, passiveAba: 1000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/7/77/PTS_Sasuke_.png/revision/latest?cb=20250215030949' },
+    { name: 'Android Vegeta', baseCost: 2000000, abaPerClick: 20000, passiveAba: 2000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/8/84/Vegeta_Skin_1.png/revision/latest?cb=20230402084519' },
+    { name: 'PTS Zoro', baseCost: 5000000, abaPerClick: 20000, passiveAba: 10000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/b/b8/Zoro.PNG/revision/latest?cb=20200325195704' },
+    { name: 'TS Zoro', baseCost: 5000000, abaPerClick: 50000, passiveAba: 5000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/4/48/ZoroS1.png/revision/latest?cb=20220827000102' },
+    { name: 'Saitama', baseCost: 10000000, abaPerClick: 100000, passiveAba: 10000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/1/12/Screen_Shot_2021-03-24_at_1.09.40_PM.png/revision/latest?cb=20210324171208' },
+    { name: 'Tanjiro', baseCost: 20000000, abaPerClick: 200000, passiveAba: 20000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/6/61/Tanjiro.png/revision/latest?cb=20200824190824' },
+    { name: 'Nanami', baseCost: 100000000, abaPerClick: 2500000, passiveAba: 100000, image: 'https://static.wikia.nocookie.net/animebattlearenaaba/images/c/c9/Nanamibase.png/revision/latest?cb=20241226040857' },
 
 ];
 
